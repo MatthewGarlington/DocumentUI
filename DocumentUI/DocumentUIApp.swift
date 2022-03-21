b8221fd33654200f2e150091cee02fe9565e7a98
Matthew Garlington <61671107+MatthewGarlington@users.noreply.github.com>
2022-03-21 11:44:38 -0400

    Initial Commit

---
+++ DocumentUI/DocumentUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentUIApp.swift
+//  DocumentUI
+//
+//  Created by Matthew Garlington on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct DocumentUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
